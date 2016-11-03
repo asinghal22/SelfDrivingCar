160e6e99ef26a8fa249bab676870f90f21680960
asinghal22 <abhishek.singhal@MBASJ-144.local>
2016-11-02 23:00:03 -0700

    modified to use pipeline

--- Project1/project1.py
+++ Project1/project1.py
@@ -1,153 +1,189 @@
+import math
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 import cv2
 
 
+
+
+#################
+
+
+def grayscale(img):
+    """Applies the Grayscale transform
+    This will return an image with only one color channel
+    but NOTE: to see the returned image as grayscale
+    you should call plt.imshow(gray, cmap='gray')"""
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+def canny(img, low_threshold, high_threshold):
+    """Applies the Canny transform"""
+    return cv2.Canny(img, low_threshold, high_threshold)
+
+def gaussian_blur(img, kernel_size):
+    """Applies a Gaussian Noise kernel"""
+    return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
+
+def region_of_interest(img, vertices):
+    """
+    Applies an image mask.
+    
+    Only keeps the region of the image defined by the polygon
+    formed from `vertices`. The rest of the image is set to black.
+    """
+    #defining a blank mask to start with
+    mask = np.zeros_like(img)   
+    
+    #defining a 3 channel or 1 channel color to fill the mask with depending on the input image
+    if len(img.shape) > 2:
+        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
+        ignore_mask_color = (255,) * channel_count
+    else:
+        ignore_mask_color = 255
+        
+    #filling pixels inside the polygon defined by "vertices" with the fill color    
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+    
+    #returning the image only where mask pixels are nonzero
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+
+
+def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
+    """
+    NOTE: this is the function you might want to use as a starting point once you want to 
+    average/extrapolate the line segments you detect to map out the full
+    extent of the lane (going from the result shown in raw-lines-example.mp4
+    to that shown in P1_example.mp4).  
+    
+    Think about things like separating line segments by their 
+    slope ((y2-y1)/(x2-x1)) to decide which segments are part of the left
+    line vs. the right line.  Then, you can average the position of each of 
+    the lines and extrapolate to the top and bottom of the lane.
+    
+    This function draws `lines` with `color` and `thickness`.    
+    Lines are drawn on the image inplace (mutates the image).
+    If you want to make the lines semi-transparent, think about combining
+    this function with the weighted_img() function below
+    """
+    ## Iterate over the output "lines" and draw lines on a blank image
+    left_avg  = 0
+    right_avg = 0
+
+    l = 0
+    r = 0
+    left_pts = []
+    right_pts= []
+
+    for line in lines: 
+        for x1,y1,x2,y2 in line:
+            slope = (y2 - y1)/(x2 - x1)
+
+            if (abs(slope) < 0.4):
+               continue
+
+            if slope < 0:
+              left_avg = left_avg+slope
+              left_pts.append((x1,y1))
+              left_pts.append((x2,y2))
+            else: 
+              right_avg = right_avg+slope
+              right_pts.append((x1,y1))
+              right_pts.append((x2,y2))
+
+
+    sorted_left = sorted(left_pts,key=lambda tup: tup[1])
+    sorted_right    = sorted(right_pts,key=lambda tup: tup[1])
+
+    left  = np.array(sorted_left)
+    right = np.array(sorted_right)
+
+
+    lx = left[:,0]
+    ly = left[:,1]
+
+    lz = np.polyfit(lx,ly,1)
+    lf = np.poly1d(lz)
+
+    lx_new = np.linspace(lx[0],lx[-1],50,dtype=int)
+    ly_new = lf(lx_new).astype(int)
+
+    rx = right[:,0]
+    ry = right[:,1]
+
+    rz = np.polyfit(rx,ry,1)
+    rf = np.poly1d(rz)
+
+    rx_new = np.linspace(rx[0],rx[-1],50,dtype=int)
+    ry_new = rf(rx_new).astype(int)
+
+
+    left_lines  = list(zip(lx_new,ly_new))
+    right_lines = list(zip(rx_new,ry_new))
+
+
+    for indx in range (1,50):
+        cv2.line(img,left_lines[indx-1], left_lines[indx],color,thickness)
+        cv2.line(img,right_lines[indx-1],right_lines[indx],color,thickness)
+
+
+def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
+    """
+    `img` should be the output of a Canny transform.
+        
+    Returns an image with hough lines drawn.
+    """
+    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
+    line_img = np.zeros((*img.shape, 3), dtype=np.uint8)
+    draw_lines(line_img, lines)
+    return line_img
+
+# Python 3 has support for cool math symbols.
+
+def weighted_img(img, initial_img, α=0.8, β=1., λ=0.):
+    """
+    `img` is the output of the hough_lines(), An image with lines drawn on it.
+    Should be a blank image (all black) with lines drawn on it.
+    
+    `initial_img` should be the image before any processing.
+    
+    The result image is computed as follows:
+    
+    initial_img * α + img * β + λ
+    NOTE: initial_img and img must be the same shape!
+    """
+    return cv2.addWeighted(initial_img, α, img, β, λ)
+
+
+
+
 # Read in and grayscale the image
 # Note: in the previous example we were reading a .jpg 
 # Here we read a .png and convert to 0,255 bytescale
-#image = (mpimg.imread('exit_ramp.png')*255).astype('uint8')
-image = (mpimg.imread('test_images/solidWhiteRight.jpg')).astype('uint8')
-gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-#plt.imshow(gray,cmap='gray')
-#plt.show()
 
-## Define a kernel size and apply Gaussian smoothing
-kernel_size = 5
-blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
-#plt.imshow(blur_gray,cmap='gray')
-#plt.show()
-
-
-#
-## Define our parameters for Canny and apply
-low_threshold = 90
-high_threshold = 180
-edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
-
-## Next we'll create a masked edges image using cv2.fillPoly()
-mask = np.zeros_like(edges)   
-
-ignore_mask_color = 255   
-
-## This time we are defining a four sided polygon to mask
-imshape = image.shape
-
-vertices = np.array([[(0,imshape[0]),((imshape[1]/2)-1, imshape[0]/2),((imshape[1]/2)+1,imshape[0]/2),(imshape[1],imshape[0])]], dtype=np.int32)
-
-
-cv2.fillPoly(mask, vertices, ignore_mask_color)
-masked_edges = cv2.bitwise_and(edges, mask)
-
-
-## Define the Hough transform parameters
-## Make a blank the same size as our image to draw on
-
+kernel_size     = 5
+low_threshold   = 90
+high_threshold  = 180
 rho             = 1 		   # distance resolution in pixels of the Hough grid
 theta           = np.pi/180 	   # angular resolution in radians of the Hough grid
 threshold       = 8    	   	   # minimum number of votes (intersections in Hough grid cell)
-min_line_length = 25 		   # minimum number of pixels making up a line
+min_line_length = 10		   # minimum number of pixels making up a line
 max_line_gap    = 10   		   # maximum gap in pixels between connectable line segments
-line_image 	= np.copy(image)*0 # creating a blank to draw lines on
+ignore_mask_color = 255   
 
-## Run Hough on edge detected image
-## Output "lines" is an array containing endpoints of detected line segments
+image = (mpimg.imread('test_images/whiteCarLaneSwitch.jpg')).astype('uint8')
+gray = grayscale(image)
 
-# Draw the lines on the edge image
-lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
+blur_gray = gaussian_blur(gray, kernel_size)
+edges     = canny(blur_gray,low_threshold,high_threshold)
+imshape   = image.shape
+vertices  = np.array([[(0,imshape[0]),((imshape[1]/2)-1, imshape[0]/2),((imshape[1]/2)+1,imshape[0]/2),(imshape[1],imshape[0])]], dtype=np.int32)
 
-## Create a "color" binary image to combine with line image
-color_edges = np.dstack((edges, edges, edges)) 
+masked_edges = region_of_interest(edges, vertices)
 
-## Iterate over the output "lines" and draw lines on a blank image
-left_avg  = 0
-right_avg = 0
+line_image = hough_lines(masked_edges, rho, theta, threshold, min_line_length, max_line_gap)
+final_img  = weighted_img(line_image, image, 0.8, 1, 0)
 
-l = 0
-r = 0
-left_pts = []
-right_pts= []
-
-for line in lines: 
-    for x1,y1,x2,y2 in line:
-        slope = (y2 - y1)/(x2 - x1)
-
-        if (abs(slope) < 0.4):
-           continue
-
-        #print   (x1, y1, x2, y2, slope)
-
-        if slope < 0:
-           left_avg = left_avg+slope
-           left_pts.append((x1,y1))
-           left_pts.append((x2,y2))
-           l = l + 1
-        else: 
-           right_avg = right_avg+slope
-           right_pts.append((x1,y1))
-           right_pts.append((x2,y2))
-           r = r + 1
-
-
-sorted_left = sorted(left_pts,key=lambda tup: tup[1])
-sorted_right = sorted(right_pts,key=lambda tup: tup[1])
-left  = np.array(sorted_left)
-right = np.array(sorted_right)
-
-#plt.plot(left[:,0],left[:,1],'ro')
-#plt.plot(right[:,0],right[:,1],'ro')
-#plt.xlim(0,imshape[1])
-#plt.ylim(0,imshape[0])
-#plt.gca().invert_yaxis()
-#plt.show()
-
-
-
-lx = left[:,0]
-ly = left[:,1]
-
-lz = np.polyfit(lx,ly,1)
-lf = np.poly1d(lz)
-
-lx_new = np.linspace(lx[0],lx[-1],50)
-ly_new = lf(lx_new)
-
-
-	
-
-rx = right[:,0]
-ry = right[:,1]
-
-
-
-rz = np.polyfit(rx,ry,1)
-rf = np.poly1d(rz)
-
-rx_new = np.linspace(rx[0],rx[-1],50)
-ry_new = rf(rx_new)
-
-left_lines = np.dstack((lx_new,ly_new))
-right_lines = np.dstack((rx_new,ry_new))
-
-
-for indx in range (1,len(left_lines)):
-    cv2.line(line_image,left_lines[indx-1][0],left_lines[indx-1][1], left_lines[indx][0], left_lines[indx][1])
-    cv2.line(line_image,right_lines[indx-1][0],right_lines[indx-1][1], right_lines[indx][0], right_lines[indx][1])
-
-lines_edges = cv2.addWeighted(color_edges, 0.8, line_image, 1, 0) 
-plt.imshow(lines_edges)
+plt.imshow(final_img)
 plt.show()
-
-#plt.plot(lx,ly,'o',lx_new,ly_new)
-#plt.plot(rx,ry,'o',rx_new,ry_new)
-#plt.xlim(0,imshape[1])
-#plt.ylim(0,imshape[0])
-#plt.gca().invert_yaxis()
-plt.show()
-
-
-#left_line  = cv2.fitLine(left,cv2.DIST_L2,0,0.01,0.01)
-#
-#print (left_line)
-#print (left_avg / l, right_avg / r)
